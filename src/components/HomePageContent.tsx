30b9be1f59f745c010eab3cbbaccd8b61a0e8739
Jordan Trpeski <viralstark@gmail.com>
2024-07-04 13:20:25 +0200

    Layouting

--- src/components/HomePageContent.tsx
+++ src/components/HomePageContent.tsx
@@ -1,22 +1,36 @@
 import React from "react";
 import Calendar from "./Calendar";
 import Footer from "./Footer";
+import WaterIntake from "./WaterIntake";
+
+
+const formatDate = () => {
+  const date = new Date();
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+  return date.toLocaleDateString(undefined, options);
+};
 
 const HomePageContent = () => {
+  const currentDate = formatDate();
 
   return (
-    <div>
-          <div className='flex items-center justify-evenly '>
-            <div className="bg-gray-400 rounded-full p-2">
-          <img className="w-6 h-6" src="/svg/calendar.svg"></img>
-          </div>
-          <div className="text-black">## Month Day</div>
-
-          <div className="bg-gray-400 rounded-full p-2">
-          <div className="bolded text-[24px] text-black">...</div>
-          </div>
-          </div> 
-         <Footer></Footer>
+    <div className=" mt-2">
+      <div className="flex items-center justify-evenly ">
+        <div className="bg-gray-400 rounded-full p-2">
+          <img className="w-6 h-6" src="/svg/calendar.svg" alt="Calendar" />
+        </div>
+        <div className="text-black">{currentDate}</div>
+        <div className="bg-gray-400 rounded-full p-2">
+          <img className="w-6 h-6" src="/svg/dot.svg" alt="Dot" />
+        </div>
+      </div>
+        <WaterIntake></WaterIntake>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/WaterIntake.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const WaterIntake = () => {
+    const [intake, setIntake] = useState(0);
+
+    const increment = () => setIntake(intake + 1);
+    const decrement = () => setIntake(intake > 0 ? intake - 1 : 0);
+
+    return (
+<div className="relative h-[75px] flex justify-between bg-gray-100 mx-4 rounded-lg mt-20 px-4">
+    <div className="absolute left-0 top-0 bottom-0 bg-gradient-to-r from-[#3D60F4] to-[#577EFE] w-[10px] rounded-l-lg"></div>
+    <div className="flex flex-col justify-between pl-4">
+        <div className="flex">
+            <div className="text-[#3d60f4] text-[18px] mr-2">Water</div>
+            <div className="text-black text-[18px]">0.9L (75%)</div>
+        </div>
+        <div className="flex space-x-1 mb-2">
+            <img className="w-4 h-4" src="/svg/waterfull.svg" alt="Water" />
+            <img className="w-4 h-4" src="/svg/waterhalf.svg" alt="Water" />
+            <img className="w-4 h-4" src="/svg/waterempty.svg" alt="Water" />
+        </div>
+    </div>
+    <div className="flex items-center space-x-4">
+        <div
+            className="text-white text-[22px] font-bold bg-[#50CE3B] p-1 rounded-full cursor-pointer"
+            onClick={decrement}
+        >
+            <img className="w-4 h-4" src="/svg/minus.svg" alt="Minus" />
+        </div>
+        <div className="text-[#9DA8C3] text-[22px] ">{intake}</div>
+        <div
+            className="text-white text-[22px] font-bold bg-[#50CE3B] p-1 rounded-full cursor-pointer"
+            onClick={increment}
+        >
+            <img className="w-4 h-4" src="/svg/plus.svg" alt="Plus" />
+        </div>
+    </div>
+</div>
+    );
+};
+
+export default WaterIntake;
